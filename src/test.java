f38301629f612dc75435bff504b0864bec27cb18
LilyZam <146707359+LilyZam@users.noreply.github.com>
2025-06-03 14:34:29 +0200

    Update test.java
    Cats and Dogs

--- src/test.java
+++ src/test.java
@@ -2,8 +2,8 @@ public class test {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello Dog");
-        System.out.println("Hello Cat");
+        System.out.println("Hello Dogs");
+        System.out.println("Hello Cats");
 
         System.out.println("Hello World!");
     }
